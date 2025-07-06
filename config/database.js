95033ce84a1d80d7ff941ce6aa5cffe1ba5b687c
Saurav Raj <er.sauravraj12@gmail.com>
2025-07-07 02:05:12 +0530

    module created first

--- config/database.js
+++ config/database.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
+require("dotenv").config();
+
 const dbConnect = () =>{
     mongoose.connect(process.env.DATABASE_URL)
+    .then(()=>console.log('DB ka connection Successfull hua'))
+    .catch((error) => {
+        console.log('Issue in DB Connection');
+        console.log(error.message);
+        process.exit(1);
+    })
 }
+module.exports = dbConnect;
