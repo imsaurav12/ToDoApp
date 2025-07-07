cc58bfe3da549684b25906f7dd03cab8561b3dc2
Saurav Raj <er.sauravraj12@gmail.com>
2025-07-07 20:33:50 +0530

    created post request

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen(3000,() => {
-    console.log('App is Running Successfully!');
+//load config from env file
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+//MIDDILEWARE TO PARSE JSON REQUEST BODY
+app.use(express.json());
+
+//import routes for TODO API
+const todoRoutes = require("./routes/todos");
+
+//mount the todo ASPI routes
+app.use("/api/v1", todoRoutes);
+
+//start server
+app.listen(PORT, () => {
+    console.log(`Server Started successfully at ${PORT}`);
 })
 
+//connect to the DataBase
+const dbConnect = require("./config/database");
+dbConnect();
+
+//default Route
+app.get("/", (req,res) => {
+    res.send('<h1> This is my HomePage baby </h1>');
+})
